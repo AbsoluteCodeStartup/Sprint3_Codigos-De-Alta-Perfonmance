55e8708f9b8783c2cdddcf5f0e7b41836a4bc5dc
FabioHNardella <fabiohnardella@gmail.com>
2025-08-13 21:24:16 -0300

    Finalizado primeira opcao do menu + iniciado terceira opcao

---
+++ src/aplicacao/Onboarding.java
@@ -0,0 +1,119 @@
+package aplicacao;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+import listas.ListaCrescenteInt;
+import listas.ListaEvolucao;
+import modelos.Colaborador;
+
+public class Onboarding {
+    public static Scanner le = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        ListaEvolucao lista = new ListaEvolucao();
+        Scanner le = new Scanner(System.in);
+        geraLista(lista);
+
+        int opcao;
+        do {
+            System.out.println("0 - Encerrar atendimento");
+            System.out.println("1 - Obter colaboradores com piores notas presentes na lista");
+            System.out.println("2 - Atualizar nota de um colaborador");
+            System.out.println("3 - Inserir novo colaborador");
+            System.out.print("Opcao: ");
+            opcao = le.nextInt();
+            switch (opcao) {
+                case 0:
+                    break;
+                case 1:
+                    obterPioresNotasNaLista(lista);
+                    break;
+                case 2:
+                    atualizarNotaColaborador(lista);
+                    break;
+                case 3:
+                    inserirNovoColaborador(lista);
+                    break;
+                default:
+                    System.out.println("Opcao Invalida");
+            }
+
+        } while (opcao != 0);
+
+        le.close();
+
+    }
+
+    private static void obterPioresNotasNaLista(ListaEvolucao lista) {
+        System.out.print("Quantas notas deseja pegar da lista?: ");
+        int quantidadeDeNotas = le.nextInt();
+        while(quantidadeDeNotas > lista.tamanho()){
+            System.out.print("Quantidade desejada maior que a lista, por favor insira um número menor: ");
+            quantidadeDeNotas = le.nextInt();
+        }
+        Colaborador[] colaboradores = lista.getColaborador(quantidadeDeNotas);
+
+        System.out.println("----------Colaboradores com piores notas----------");
+        for (int i = 0; i < quantidadeDeNotas; i++) {
+            System.out.println("ID: " + colaboradores[i].getID() + " | Nome: " + colaboradores[i].getNome() + " | Nota: " + colaboradores[i].getNota());
+        }
+        System.out.println("--------------------------------------------------\n\n");
+
+    }
+
+    //TODO
+    private static void atualizarNotaColaborador(ListaEvolucao lista) {
+    }
+    //TODO
+    private static void inserirNovoColaborador(ListaEvolucao lista) {
+        System.out.print("ID: ");
+        int id = le.nextInt();
+        System.out.print("Nome: ");
+        String nome = le.next();
+        System.out.print("Setor: ");
+        String setor = le.next();
+        System.out.print("Buddy: ");
+        String buddy = le.next();
+
+
+        lista.add(new Colaborador(id, nome, setor, buddy,-1));
+    }
+
+
+
+    public static void geraLista(ListaEvolucao lista) {
+        /*Altere esse metodo para inserir um objeto da classe Colaborador na lista*/
+        String caminhoDoArquivo = "src/arquivos/Colaboradores.txt";
+
+        try {
+            // Criar um objeto File com o caminho do arquivo
+            File arquivo = new File(caminhoDoArquivo);
+
+            // Criar um Scanner para ler o arquivo
+            Scanner leArq = new Scanner(arquivo);
+
+            // Loop para ler linha por linha at� o final do arquivo
+            while (leArq.hasNextLine()) {
+                // Ler a pr�xima linha
+                String linha = leArq.nextLine();
+                String[] partes = linha.split(";");
+                int id = Integer.parseInt(partes[0]);
+                String nome = partes[1];
+                String setor = partes[2];
+                String buddy = partes[3];
+                int nota = Integer.parseInt(partes[4]);
+                Colaborador colaborador = new Colaborador(id, nome, setor, buddy, nota);
+                lista.add(colaborador);
+            }
+            // Fechar o objeto da classe Scanner le
+            leArq.close();
+        } catch (FileNotFoundException e) {
+            // Caso o arquivo n�o seja encontrado
+            System.out.println("Arquivo n�o encontrado: " + e.getMessage());
+        }
+    }
+
+
+}
---
+++ src/listas/ListaEvolucao.java
@@ -0,0 +1,103 @@
+package listas;
+
+import modelos.Colaborador;
+
+public class ListaEvolucao {
+
+    private class NO {
+        Colaborador colaborador;
+        NO prox;
+    }
+
+    NO lista = null;
+
+    public boolean isEmpty() {
+        return (lista == null);
+    }
+
+    public void add(Colaborador colaboradorASerAdicionado) {
+        NO novo = new NO();
+        novo.colaborador = colaboradorASerAdicionado;
+        if (isEmpty()) {
+            lista = novo;
+            novo.prox = null;
+        } else if (novo.colaborador.getNota() < lista.colaborador.getNota()) {
+            novo.prox = lista;
+            lista = novo;
+        } else if (novo.colaborador.getNota() == lista.colaborador.getNota()) {
+            if (novo.colaborador.getID() < lista.colaborador.getID()) {
+                novo.prox = lista;
+                lista = novo;
+            } else {
+                lista.prox = novo;
+                novo = lista;
+            }
+        } else {
+            NO aux = lista;
+            boolean achou = false;
+            while (aux.prox != null && !achou) {
+                if (aux.prox.colaborador.getNota() < novo.colaborador.getNota())
+                    aux = aux.prox;
+                else
+                    achou = true;
+            }
+            novo.prox = aux.prox;
+            aux.prox = novo;
+        }
+    }
+
+    public void show() {
+        NO aux = lista;
+        System.out.println("*********** Lista ************");
+        while (aux != null) {
+            System.out.print(aux.colaborador + "\t");
+            aux = aux.prox;
+        }
+        System.out.println();
+    }
+
+    public boolean remove(int id) {
+        boolean achou = false;
+        if (!isEmpty()) {
+            if (id == lista.colaborador.getID()) {
+                achou = true;
+                lista = lista.prox;
+            } else {
+                NO aux = lista;
+                while (aux.prox != null && !achou) {
+                    if (id == aux.prox.colaborador.getID())
+                        achou = true;
+                    else
+                        aux = aux.prox;
+                }
+                if (achou)
+                    aux.prox = aux.prox.prox;
+            }
+        }
+        return achou;
+    }
+
+    public Colaborador[] getColaborador(int quantidadeDeVezes) {
+        NO aux = lista;
+        Colaborador[] colaboradores = new Colaborador[quantidadeDeVezes];
+        for (int i = 0; i < quantidadeDeVezes; i++) {
+            if(aux.colaborador.getNota() == -1){
+                i++;
+            }
+            colaboradores[i] = aux.colaborador;
+            aux = aux.prox;
+        }
+        return colaboradores;
+    }
+
+    public int tamanho() {
+        NO aux = lista;
+        int tamanho = 0;
+        while (aux.prox != null) {
+            tamanho++;
+            aux = aux.prox;
+        }
+        return tamanho;
+    }
+
+}
